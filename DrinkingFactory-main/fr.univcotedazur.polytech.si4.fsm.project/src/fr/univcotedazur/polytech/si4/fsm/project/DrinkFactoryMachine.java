c32b90b7bb6da297904d3575cca679ba79a322e9
keyanfang <61821753+keyanfang@users.noreply.github.com>
2020-11-06 18:52:15 +0100

    Add files via upload

---
+++ DrinkingFactory-main/fr.univcotedazur.polytech.si4.fsm.project/src/fr/univcotedazur/polytech/si4/fsm/project/DrinkFactoryMachine.java
@@ -0,0 +1,624 @@
+package fr.univcotedazur.polytech.si4.fsm.project;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JSeparator;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import fr.univcotedazur.polytech.si4.fsm.project.drink.DrinkStatemachine;
+import fr.univcotedazur.polytech.si4.fsm.project.drink.IDrinkStatemachine.SCInterfaceListener;
+
+
+public class DrinkFactoryMachine extends JFrame {
+	JLabel messagesToUser;
+	JSlider sugarSlider;
+	JSlider sizeSlider;
+	JSlider temperatureSlider;
+	
+	JButton coffeeButton;
+	JButton expressoButton;
+	JButton teaButton;
+	JButton money50centsButton;
+	JButton money25centsButton;
+	JButton money10centsButton;
+	JButton nfcBiiiipButton;
+	
+	int drinkPrice; 
+	int paidCoinsValue;
+	int myCoin;
+	int refund;
+	boolean startPrepare = false; // TODO 
+	
+	
+	public void initialDrinkButton() { 
+		coffeeButton.setBackground(Color.DARK_GRAY);
+		expressoButton.setBackground(Color.DARK_GRAY);
+		teaButton.setBackground(Color.DARK_GRAY);
+	}
+	
+	public void initialSliders() { 
+		sugarSlider.setValue(0);
+		sizeSlider.setValue(1);
+		temperatureSlider.setValue(2);
+	}
+	
+	public void myWait(int myTime) { 
+		try {
+			TimeUnit.MILLISECONDS.sleep(myTime);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	 
+	class DrinkInterfaceImplementation implements SCInterfaceListener {
+		DrinkFactoryMachine thedm;
+		
+		public DrinkInterfaceImplementation(DrinkFactoryMachine dm) {
+			thedm = dm;
+		}
+		
+		@Override
+		public void onWaitCoinRaised() {
+			messagesToUser.setText("Please choose your drink");
+
+			myWait(500);
+			paidCoinsValue += myCoin;
+			if(drinkPrice!=0) {
+				onComfirmCoinsRaised();
+			} 	
+		}
+
+		void cleanInfos() {
+			initialDrinkButton();
+			initialSliders();
+			nfcBiiiipButton.setBackground(Color.DARK_GRAY);
+			drinkPrice = 0;
+			paidCoinsValue = 0;
+			refund = 0;
+			startPrepare = false;
+		}
+		
+		@Override
+		public void onInitialRaised() { 
+			messagesToUser.setText("<html>You have not operated for 45 seconds<br>"
+					+ "The order has been canceled");
+			if(paidCoinsValue>0) {
+				messagesToUser.setText("<html>You have not operated for 45 seconds<br>"
+						+ "The order has been canceled <br>Your coins of 0."+paidCoinsValue+"€ have been returned");
+			}
+			cleanInfos();
+		}
+
+		@Override
+		public void onCancelOrderRaised() {
+			messagesToUser.setText("<html>You canceled the order<br>Your coins of 0."+paidCoinsValue+"€ have been returned");
+			cleanInfos();
+		}
+
+		@Override
+		public void onComfirmCoinsRaised() {
+			refund = paidCoinsValue - drinkPrice;
+			if(refund>0) {
+				messagesToUser.setText("<html>Payment is successful, start to make drinks<br>You will get a refund of 0."+refund+"€");
+				theFSM.raiseOrderSuccess();
+			}else if(refund==0) {
+				messagesToUser.setText("<html>Payment is successful, start to make drinks");
+				theFSM.raiseOrderSuccess();
+			}else if(refund<0) {
+				messagesToUser.setText("<html>You still need to pay "+(-0.01*refund)+"€");
+			}
+		}
+
+		@Override
+		public void onCleanMachineRaised() {
+			messagesToUser.setText("<html>Machine is cleaned");
+			cleanInfos();
+			sugarSlider.setEnabled(true);
+			sizeSlider.setEnabled(true);
+			temperatureSlider.setEnabled(true);
+		}
+
+		@Override
+		public void onBarRaised() {
+			JProgressBar progressBar= new JProgressBar();
+			Timer timer = new Timer();
+			TimerTask Task1 = new TimerTask() {
+				@Override
+				public void run() {
+					progressBar.setValue(14);
+				}
+				
+			};
+			TimerTask Task2 = new TimerTask() {
+				@Override
+				public void run() {
+					progressBar.setValue(72);
+				}
+				
+			};
+			TimerTask Task3 = new TimerTask() {
+				@Override
+				public void run() {
+					progressBar.setValue(100);
+				}
+				
+			};
+			TimerTask Task4 = new TimerTask() {
+				@Override
+				public void run() {
+					progressBar.setValue(3);
+				}
+				
+			};
+			TimerTask Task5 = new TimerTask() {
+				@Override
+				public void run() {
+					progressBar.setValue(13);
+				}
+				
+			};
+			TimerTask Task6 = new TimerTask() {
+				@Override
+				public void run() {
+					progressBar.setValue(16);
+				}
+				
+			};
+			TimerTask Task7 = new TimerTask() {
+				@Override
+				public void run() {
+					progressBar.setValue(91);
+				}
+				
+			};
+			switch(drinkPrice) {
+			case 35:
+				timer.schedule(Task1, 1000);
+				timer.schedule(Task2, 6000);
+				timer.schedule(Task3, 7000);
+				break;
+			
+			case 40:
+				timer.schedule(Task1, 1000);
+				timer.schedule(Task2, 6000);
+				timer.schedule(Task3, 7000);
+				break;
+			case 50:
+				timer.schedule(Task4, 1000);
+				timer.schedule(Task5, 6000);
+				timer.schedule(Task6, 1000);
+				timer.schedule(Task7, 37000);
+				timer.schedule(Task3, 40000);
+				break;
+			// TODO Auto-generated method stub
+			}
+		}
+
+		@Override
+		public void onPrepStartRaised() {
+			startPrepare = true;
+			switch(drinkPrice) {
+				case 35:
+					theFSM.raiseIsCoffee();
+					break;
+				case 50:
+					theFSM.raiseIsEspresso();
+					break;
+				case 40:
+					theFSM.raiseIsTea();
+					break;
+			} 
+			sugarSlider.setEnabled(false);
+			sizeSlider.setEnabled(false);
+			temperatureSlider.setEnabled(false);
+		}
+
+		@Override
+		public void onPrepFinishRaised() {
+			// TODO Auto-generated method stub
+			//进度条结束
+			messagesToUser.setText("<html>Your drink is ready");
+		}
+
+		@Override
+		public void onIsActiveRaised() {
+			// nothing
+		}
+		
+		@Override
+		public void onWaterReadyRaised() {
+			// nothing
+		}
+
+		@Override
+		public void onCupReadyRaised() {
+			// nothing
+		}
+	}
+	
+	
+	protected DrinkStatemachine theFSM;
+	
+	private static final long serialVersionUID = 2030629304432075314L;
+	private JPanel contentPane;
+	/**
+	 * @wbp.nonvisual location=311,475
+	 */
+	//private final ImageIcon imageIcon = new ImageIcon();
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DrinkFactoryMachine frame = new DrinkFactoryMachine();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public DrinkFactoryMachine() {
+		
+		theFSM = new DrinkStatemachine();
+	    TimerService timer = new TimerService();
+	    theFSM.setTimer(timer);
+	    theFSM.init();
+	    theFSM.enter();
+	    theFSM.getSCInterface().getListeners().add(
+				new DrinkInterfaceImplementation(this));
+
+		
+		setForeground(Color.WHITE);
+		setFont(new Font("Cantarell", Font.BOLD, 22));
+		setBackground(Color.DARK_GRAY);
+		setTitle("Drinking Factory Machine");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 650, 650);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.DARK_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		messagesToUser = new JLabel("<html>This is<br>place to communicate <br> with the user");
+		messagesToUser.setForeground(Color.WHITE);
+		messagesToUser.setHorizontalAlignment(SwingConstants.LEFT);
+		messagesToUser.setVerticalAlignment(SwingConstants.TOP);
+		messagesToUser.setToolTipText("message to the user");
+		messagesToUser.setBackground(Color.WHITE);
+		messagesToUser.setBounds(126, 34, 165, 175);
+		contentPane.add(messagesToUser);
+
+		JLabel lblCoins = new JLabel("Coins");
+		lblCoins.setForeground(Color.WHITE);
+		lblCoins.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCoins.setBounds(538, 12, 44, 15);
+		contentPane.add(lblCoins);
+
+		coffeeButton = new JButton("Coffee");
+		coffeeButton.setForeground(Color.WHITE);
+		coffeeButton.setBackground(Color.DARK_GRAY);
+		coffeeButton.setBounds(12, 34, 96, 25);
+		contentPane.add(coffeeButton);
+		coffeeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(startPrepare)
+            		return;
+            	initialDrinkButton();
+            	drinkPrice = 35;
+            	messagesToUser.setText("<html>Please pay 0.35€ for coffee");
+            	coffeeButton.setBackground(Color.green);
+            	theFSM.raiseChooseDrink();
+            }
+        });
+
+		expressoButton = new JButton("Expresso");
+		expressoButton.setForeground(Color.WHITE);
+		expressoButton.setBackground(Color.DARK_GRAY);
+		expressoButton.setBounds(12, 71, 96, 25);
+		contentPane.add(expressoButton);
+		expressoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(startPrepare)
+            		return;
+            	initialDrinkButton();
+            	drinkPrice = 50;
+            	messagesToUser.setText("<html>Please pay 0.50€ for expresso");
+            	expressoButton.setBackground(Color.green);
+            	theFSM.raiseChooseDrink();
+            }
+        });
+
+		teaButton = new JButton("Tea");
+		teaButton.setForeground(Color.WHITE);
+		teaButton.setBackground(Color.DARK_GRAY);
+		teaButton.setBounds(12, 108, 96, 25);
+		contentPane.add(teaButton);
+		teaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(startPrepare)
+            		return;
+            	initialDrinkButton();
+            	drinkPrice = 40;
+            	messagesToUser.setText("<html>Please pay 0.40€ for tea");
+            	teaButton.setBackground(Color.green);
+            	theFSM.raiseChooseDrink();
+            }
+        });
+
+		JButton soupButton = new JButton("Soup");
+		soupButton.setForeground(Color.WHITE);
+		soupButton.setBackground(Color.DARK_GRAY);
+		soupButton.setBounds(12, 145, 96, 25);
+		contentPane.add(soupButton);
+
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setStringPainted(true);
+		progressBar.setValue(10);
+		progressBar.setForeground(Color.LIGHT_GRAY);
+		progressBar.setBackground(Color.DARK_GRAY);
+		progressBar.setBounds(12, 254, 622, 26);
+		contentPane.add(progressBar);
+
+		sugarSlider = new JSlider();
+		sugarSlider.setValue(1);
+		sugarSlider.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		sugarSlider.setBackground(Color.DARK_GRAY);
+		sugarSlider.setForeground(Color.WHITE);
+		sugarSlider.setPaintTicks(true);
+		sugarSlider.setMinorTickSpacing(1);
+		sugarSlider.setMajorTickSpacing(1);
+		sugarSlider.setMaximum(4);
+		sugarSlider.setBounds(301, 51, 200, 36);
+		contentPane.add(sugarSlider);
+		sugarSlider.addChangeListener(new ChangeListener() {
+		      public void stateChanged(ChangeEvent event) {
+		    	  if(startPrepare)
+	            		return;
+		    	  theFSM.raiseChooseSlide();
+		      }
+		});
+
+		sizeSlider = new JSlider();
+		sizeSlider.setPaintTicks(true);
+		sizeSlider.setValue(1);
+		sizeSlider.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		sizeSlider.setBackground(Color.DARK_GRAY);
+		sizeSlider.setForeground(Color.WHITE);
+		sizeSlider.setMinorTickSpacing(1);
+		sizeSlider.setMaximum(2);
+		sizeSlider.setMajorTickSpacing(1);
+		sizeSlider.setBounds(301, 125, 200, 36);
+		contentPane.add(sizeSlider);
+		sizeSlider.addChangeListener(new ChangeListener() {
+		      public void stateChanged(ChangeEvent event) {
+		    	  if(startPrepare)
+	            		return;
+		    	  theFSM.raiseChooseSlide();
+		      }
+		});
+		
+		temperatureSlider = new JSlider();
+		temperatureSlider.setPaintLabels(true);
+		temperatureSlider.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		temperatureSlider.setValue(2);
+		temperatureSlider.setBackground(Color.DARK_GRAY);
+		temperatureSlider.setForeground(Color.WHITE);
+		temperatureSlider.setPaintTicks(true);
+		temperatureSlider.setMajorTickSpacing(1);
+		temperatureSlider.setMaximum(3);
+		temperatureSlider.setBounds(301, 188, 200, 54);
+		temperatureSlider.addChangeListener(new ChangeListener() {
+		      public void stateChanged(ChangeEvent event) {
+		    	  if(startPrepare)
+	            		return;
+		    	  theFSM.raiseChooseSlide();
+		      }
+		});
+
+		Hashtable<Integer, JLabel> temperatureTable = new Hashtable<Integer, JLabel>();
+		temperatureTable.put(0, new JLabel("20°C"));
+		temperatureTable.put(1, new JLabel("35°C"));
+		temperatureTable.put(2, new JLabel("60°C"));
+		temperatureTable.put(3, new JLabel("85°C"));
+		for (JLabel l : temperatureTable.values()) {
+			l.setForeground(Color.WHITE);
+		}
+		temperatureSlider.setLabelTable(temperatureTable);
+
+		contentPane.add(temperatureSlider);
+
+		JButton icedTeaButton = new JButton("Iced Tea");
+		icedTeaButton.setForeground(Color.WHITE);
+		icedTeaButton.setBackground(Color.DARK_GRAY);
+		icedTeaButton.setBounds(12, 182, 96, 25);
+		contentPane.add(icedTeaButton);
+
+		JLabel lblSugar = new JLabel("Sugar");
+		lblSugar.setForeground(Color.WHITE);
+		lblSugar.setBackground(Color.DARK_GRAY);
+		lblSugar.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSugar.setBounds(380, 34, 44, 15);
+		contentPane.add(lblSugar);
+
+		JLabel lblSize = new JLabel("Size");
+		lblSize.setForeground(Color.WHITE);
+		lblSize.setBackground(Color.DARK_GRAY);
+		lblSize.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSize.setBounds(380, 113, 44, 15);
+		contentPane.add(lblSize);
+
+		JLabel lblTemperature = new JLabel("Temperature");
+		lblTemperature.setForeground(Color.WHITE);
+		lblTemperature.setBackground(Color.DARK_GRAY);
+		lblTemperature.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTemperature.setBounds(363, 173, 96, 15);
+		contentPane.add(lblTemperature);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
+		lblCoins.setLabelFor(panel);
+		panel.setBounds(538, 25, 96, 97);
+		contentPane.add(panel);
+
+		money50centsButton = new JButton("0.50 €");
+		money50centsButton.setForeground(Color.WHITE);
+		money50centsButton.setBackground(Color.DARK_GRAY);
+		panel.add(money50centsButton);
+		money50centsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(startPrepare)
+            		return;
+            	myCoin = 50;
+            	theFSM.raiseChooseCoin();
+            }
+        });
+
+		money25centsButton = new JButton("0.25 €");
+		money25centsButton.setForeground(Color.WHITE);
+		money25centsButton.setBackground(Color.DARK_GRAY);
+		panel.add(money25centsButton);
+		money25centsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(startPrepare)
+            		return;
+            	myCoin = 25;
+            	theFSM.raiseChooseCoin();
+            } 
+        });
+
+		money10centsButton = new JButton("0.10 €");
+		money10centsButton.setForeground(Color.WHITE);
+		money10centsButton.setBackground(Color.DARK_GRAY);
+		panel.add(money10centsButton);
+		money10centsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(startPrepare)
+            		return;
+            	myCoin = 10;
+            	theFSM.raiseChooseCoin();
+            }
+        });
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.DARK_GRAY);
+		panel_1.setBounds(538, 154, 96, 40);
+		contentPane.add(panel_1);
+
+		nfcBiiiipButton = new JButton("biiip");
+		nfcBiiiipButton.setForeground(Color.WHITE);
+		nfcBiiiipButton.setBackground(Color.DARK_GRAY);
+		panel_1.add(nfcBiiiipButton);
+		nfcBiiiipButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(startPrepare)
+            		return;
+            	messagesToUser.setText("<html>Your NFC information is saved");
+            	theFSM.raiseChooseNFC();
+            }
+        });
+
+		JLabel lblNfc = new JLabel("NFC");
+		lblNfc.setForeground(Color.WHITE);
+		lblNfc.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNfc.setBounds(541, 139, 41, 15);
+		contentPane.add(lblNfc);
+
+		JSeparator separator = new JSeparator();
+		separator.setBounds(12, 292, 622, 15);
+		contentPane.add(separator);
+
+		JButton addCupButton = new JButton("Add cup");
+		addCupButton.setForeground(Color.WHITE);
+		addCupButton.setBackground(Color.DARK_GRAY);
+		addCupButton.setBounds(45, 336, 96, 25);
+		contentPane.add(addCupButton);
+		addCupButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(startPrepare)
+            		return;
+            	//theFSM.raiseAddCup();
+            	//TODO
+            }
+        });
+
+		BufferedImage myPicture = null;
+		try {
+			myPicture = ImageIO.read(new File("./picts/vide2.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		JLabel labelForPictures = new JLabel(new ImageIcon(myPicture));
+		labelForPictures.setBounds(175, 319, 286, 260);
+		contentPane.add(labelForPictures);
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.DARK_GRAY);
+		panel_2.setBounds(538, 217, 96, 33);
+		contentPane.add(panel_2);
+
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.setForeground(Color.WHITE);
+		cancelButton.setBackground(Color.DARK_GRAY);
+		panel_2.add(cancelButton);
+		cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(startPrepare)
+            		return;
+            	theFSM.raiseCancel();
+            }
+        });
+
+		// listeners
+		addCupButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				BufferedImage myPicture = null;
+				try {
+					myPicture = ImageIO.read(new File("./picts/ownCup.jpg"));
+				} catch (IOException ee) {
+					ee.printStackTrace();
+				}
+				labelForPictures.setIcon(new ImageIcon(myPicture));
+			}
+		});
+
+	}
+}
